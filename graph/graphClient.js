00768f77634289d52699f01d186c8dae68572aa4
waldekmastykarz <waldek@mastykarz.nl>
2021-03-25 16:31:13 +0100

    WIP: cache

--- graph/graphClient.js
+++ graph/graphClient.js
@@ -1,14 +1,56 @@
 import { getToken } from '../auth.js';
 
 // Create an authentication provider
-const authProvider = {
+function AuthProvider() {
+  nextMiddleware: undefined;
+
+  return {
     getAccessToken: async () => {
       // Call getToken in auth.js
       return await getToken();
+    },
+    setNext: (next) => {
+      this.nextMiddleware = next;
     }
-  };
+  }
+};
 
-  // Graph client singleton
-const graphClient = MicrosoftGraph.Client.initWithMiddleware({ authProvider });
+const middleware = MicrosoftGraph.MiddlewareFactory.getDefaultMiddlewareChain(new AuthProvider());
+
+function CacheMiddleware() {
+  nextMiddleware: undefined;
+
+  return {
+    execute: async (context) => {
+      const requestKey = btoa(context.request);
+      const response = window.localStorage.getItem(requestKey);
+      if (response) {
+        const resp = JSON.parse(response);
+        context.response = new Response(resp.body, resp);
+      }
+      else {
+        await this.nextMiddleware.execute(context);
+        const resp = context.response.clone();
+        console.log(resp.headers.get());
+        const response = {
+          url: resp.url,
+          status:  resp.status,
+          statusText:  resp.statusText,
+          headers: resp.headers,
+          body: await resp.json()
+        };
+        window.localStorage.setItem(requestKey, JSON.stringify(response));
+      }
+    },
+    setNext: (next) => {
+      this.nextMiddleware = next;
+    }
+  }
+};
+
+middleware.unshift(new CacheMiddleware());
+
+// Graph client singleton
+const graphClient = MicrosoftGraph.Client.initWithMiddleware({ middleware });
 
 export default graphClient;
